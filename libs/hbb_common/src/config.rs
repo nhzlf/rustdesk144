aff716109e1155e1667d32b3f2d963b2b3e212e2
nhzlf <nhzlf@163.com>
2026-01-12 23:14:44 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -62,10 +62,11 @@ lazy_static::lazy_static! {
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
-    //pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = Default::default();  //20260112
+    pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = Default::default();  //20260112
     pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
- 	pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = {
+ 	/*
+	pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
         //ID服务器，该配置部分客户端生效，读取Repository secrets值
         map.insert(
@@ -110,6 +111,7 @@ lazy_static::lazy_static! {
         map.insert("hide-tray".to_string(), "Y".to_string());
         RwLock::new(map)
     };
+	*/
 
 
 
